8d1b75f7400329b8f964268d8dad52b57c13e158
theonlineaid <nkomorfaruk.com>
2025-04-22 18:36:48 +0600

    Add cors for frontend

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"my-api/handlers"
 	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -32,6 +35,16 @@ func main() {
 
 	// Initialize Gin router
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"}, // your frontend domains
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Public routes
 	r.POST("/register", handlers.Register(db))
